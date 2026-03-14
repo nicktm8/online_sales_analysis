0887e1506345dd338175fec4455628cb50c89c9c
Nick Tem <nola234@icloud.com>
2026-03-14 19:30:54 +0100

    Add product managment methods and inventory value calculation

--- product_manager.py
+++ product_manager.py
@@ -1,6 +1,16 @@
+from product import Product
+
 class ProductManager:
     
     def __init__(self):
         self.products = []
     
-    
+    def add_product(self, product):
+        self.products.append(product)
+        
+    def display_products(self):
+        for product in self.products:
+            print(product.display_info())
+
+    def total_inventory_value(self):
+        return sum(product.price * product.quantity for product in self.products)
