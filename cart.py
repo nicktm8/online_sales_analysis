8fabd26ccc4748ac1274928c34ba6c1c152fc332
Nick Tem <nola234@icloud.com>
2026-03-16 12:09:50 +0100

    Add methods for adding products, displaying cart items and calculating total price

--- cart.py
+++ cart.py
@@ -1,5 +1,18 @@
+from product import Product
+
 class Cart:
     
     def __init__(self):
-        self.cart_intems = []
+        self.cart_items = []
     
+    def add_to_cart(self, product, quantity):
+        product.update_quantity(quantity)
+        self.cart_items.append(product)
+
+    def total_cart_price(self):
+        return sum(product.price * product.quantity for product in self.cart_items) 
+    
+    def display_cart(self):
+        for product in self.cart_items:
+            print(product.display_info())
+                 
