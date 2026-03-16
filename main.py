c407996fea26083063697ae67eecf4a923565713
Nick Tem <nola234@icloud.com>
2026-03-16 13:14:27 +0100

    Modify product data and remove inventory display from main script

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ manager = pm.ProductManager()
 # List of products
 
 products = [
-        pr.Product("Macbook Air M3", 1450, 9),
+        pr.Product("Macbook Pro M3", 1550, 15),
         pr.Product("Samsung Galaxy S25 Ultra", 1100, 10),
         pr.Product("Samsung Galaxy Tab S11", 899, 20),
         pr.Product("Dell Gaming Monitor (27 inch)", 250, 30)
@@ -20,14 +20,4 @@ manager.add_product(products[0])
 manager.add_product(products[1])
 manager.add_product(products[2])
 manager.add_product(products[3])
-
-# Display of products
-
-print("------------List of Products--------------\n")
-manager.display_products()
-
-# Calculating total inventory value
-
-total = manager.total_inventory_value()
-print(f"Total inventory value: {total:,.2f} EUR")
 
