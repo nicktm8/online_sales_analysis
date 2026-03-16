26330ed895120940746d242386c4e90f0623ebc3
Nick Tem <nola234@icloud.com>
2026-03-16 12:38:27 +0100

    Update main script to demostrate cart functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import product as pr
 import product_manager as pm
+import cart
 
 # Instance Product manager
 
@@ -23,11 +24,18 @@ manager.add_product(products[3])
 
 # Display of products
 
-print("------------List of Products--------------\n")
+print("\n------------List of Products--------------\n")
 manager.display_products()
 
 # Calculating total inventory value
-
+print("-"*20)
 total = manager.total_inventory_value()
 print(f"Total inventory value: {total:,.2f} EUR")
 
+shoping_cart = cart.Cart()
+print("\n----------Cart----------\n")
+shoping_cart.add_to_cart(manager.products[0], 1)
+shoping_cart.add_to_cart(manager.products[1], 2)
+shoping_cart.add_to_cart(manager.products[3], 3)
+
+shoping_cart.display_cart()
